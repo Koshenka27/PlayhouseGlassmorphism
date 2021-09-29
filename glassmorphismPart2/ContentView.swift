7b1e391504d612b7afc94a69e5076e73ceac138c
Logan Koshenka <koshenka27@gmail.com>
2021-09-29 10:33:44 -0400

    Initial Commit

---
+++ glassmorphismPart2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  glassmorphismPart2
+//
+//  Created by Logan Koshenka on 9/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ glassmorphismPart2/glassmorphismPart2App.swift
@@ -0,0 +1,17 @@
+//
+//  glassmorphismPart2App.swift
+//  glassmorphismPart2
+//
+//  Created by Logan Koshenka on 9/29/21.
+//
+
+import SwiftUI
+
+@main
+struct glassmorphismPart2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
